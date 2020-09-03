f8e02ebfe7c2dca2bfd72b30e626ce218b1b9b09
Daniel Oliveira <daniel.oliveira@present-technologies.com>
2020-09-03 15:06:07 +0100

    Test angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { L10nTranslationModule, L10nIntlModule, L10nLoader } from "angular-l10n";
+import { L10nTranslationModule, L10nIntlModule, L10nLoader } from 'angular-l10n';
 import { NgxsModule } from '@ngxs/store';
-import { NgxsLoggerPluginModule } from "@ngxs/logger-plugin";
+import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppState } from './state/app.state';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { environment } from 'src/environments/environment';
 import { HttpTranslationLoader } from './shared/localization/http-translation-loader';
 import { l10nConfig, initL10n } from './shared/localization/localization.config';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -33,7 +38,12 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [
     {
